2855424ae98a40a5f53853f09b5e6d7fb1607248
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-07 03:00:18 +0100

    Update Makefile.pl

--- Makefile.pl
+++ Makefile.pl
@@ -1,1 +1,8 @@
-
+package (version) distribution(s); urgency=urgency
+  [optional blank line(s), stripped]
+  * change details
+  more change details
+  [blank line(s), included in output of dpkg-parsechangelog]
+  * even more change details
+  [optional blank line(s), stripped]
+ -- maintainer seriki walter yakub <kubulee.kl@gmail.com>[two spaces]  2026
